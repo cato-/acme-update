200f0f41f642345f843ae5bfafd4f59e45698462
root <root@niob.xnis.de>
2017-11-12 12:39:21 +0100

    Removed unnecessary output

--- acme_update.sh
+++ acme_update.sh
@@ -17,7 +17,7 @@ CHANGED=0
 
 for CSR in $CERTIFICATE_DIR/*.csr; do
     CRT=${CSR%.*}.crt
-    if openssl x509 -in $CRT -noout -checkend $(( 60 * 60 * 24 * $MIN_VALID_DAYS )) 2>/dev/null; then
+    if openssl x509 -in $CRT -noout -checkend $(( 60 * 60 * 24 * $MIN_VALID_DAYS )) 2>/dev/null 2>&1; then
         continue
     fi
     $DEBUG $BIN_DIR/acme_tiny.py --quiet --account-key $ACCOUNT_KEY --csr $CSR  --acme-dir /srv/letsencrypt/ > $CRT.new || continue
