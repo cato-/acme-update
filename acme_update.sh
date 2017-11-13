84965c6d13790b14ff1d40acaec2b91a90b24381
root <root@astat.xnis.de>
2017-11-13 07:58:30 +0100

    Use system acme-tiny, if available

--- acme_update.sh
+++ acme_update.sh
@@ -5,6 +5,10 @@ MIN_VALID_DAYS=7
 ACCOUNT_KEY=/etc/letsencrypt/account.key
 ACME_DIR=/srv/letsencrypt
 PATH=$PATH:$(dirname $0)
+ACME_TINY=$(which acme-tiny)
+if [ -z "$ACME_TINY" ]; then
+    ACME_TINY=$BIN_DIR/acme_tiny.py
+fi 
 
 BIN_DIR=$(dirname $0)
 
@@ -17,10 +21,12 @@ CHANGED=0
 
 for CSR in $CERTIFICATE_DIR/*.csr; do
     CRT=${CSR%.*}.crt
-    if openssl x509 -in $CRT -noout -checkend $(( 60 * 60 * 24 * $MIN_VALID_DAYS )) 2>/dev/null 2>&1; then
-        continue
+    if [ -e $CRT ]; then
+        if openssl x509 -in $CRT -noout -checkend $(( 60 * 60 * 24 * $MIN_VALID_DAYS )) >/dev/null 2>&1; then
+            continue
+        fi
     fi
-    $DEBUG $BIN_DIR/acme_tiny.py --quiet --account-key $ACCOUNT_KEY --csr $CSR  --acme-dir /srv/letsencrypt/ > $CRT.new || continue
+    $DEBUG $ACME_TINY --quiet --account-key $ACCOUNT_KEY --csr $CSR  --acme-dir /srv/letsencrypt/ > $CRT.new || continue
     echo "Renewed certificate for ${CSR%.*}"
     $DEBUG curl -s https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem >> $CRT.new
     if [ ! -e $CERTIFICATE_DIR/old ]; then mkdir $CERTIFICATE_DIR/old; fi;
